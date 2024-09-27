7b4e1ff7aeef1545aa154289282b8ae303d4903a
putrinabella <putrinabellaaa@gmail.com>
2024-09-27 09:35:34 +0800

    feat: update detail page layout

--- src/components/DetailProduct.jsx
+++ src/components/DetailProduct.jsx
@@ -48,7 +48,7 @@ const DetailProduct = () => {
     calculatePriceWithDiscount(product.price);
 
   return (
-    <div className="card lg:card-side bg-slate-400 shadow-xl lg:m-20 m-5">
+    <div className="card lg:card-side bg-white shadow-xl lg:m-20 m-5">
       <div className="lg:w-1/3 w-full">
         <figure className="p-10">
           <img
@@ -60,25 +60,53 @@ const DetailProduct = () => {
       </div>
 
       <div className="card-body lg:w-2/3 w-full">
-        <h2 className="card-title font-bold text-2xl">{product.title}</h2>
+        <h2 className="card-title font-bold text-4xl">{product.title}</h2>
         <div className="mt-2">
-          <span className="text-lg font-bold line-through mr-2">
+          <span className="text-2xl font-bold line-through mr-2">
             ${originalPrice}
           </span>
-          <span className="text-lg font-bold mr-2">${discountedPrice}</span>
-          <span className="text-sm text-red-500">-{discountPercent}% off!</span>
+          <span className="text-3xl font-bold mr-2">${discountedPrice}</span>
+          <span className="text-lg text-red-500">-{discountPercent}% off!</span>
         </div>
         <div className="card-actions justify-start">
-          <div className="badge badge-secondary">
-            <p>⭐ {product.rating.rate}/5</p>
+          <div className="badge badge-secondary p-4">
+            <p className="text-lg">⭐ {product.rating.rate}/5</p>
           </div>
-          <div className="badge badge-outline">{product.category}</div>
+          <div className="badge badge-outline text-lg p-4">
+            {product.category}
+          </div>
         </div>
-        <div className="desc-detail">
+        <div className="desc-detail mt-3">
           <p>{product.description}</p>
         </div>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Listen</button>
+        <div className="card-actions justify-start mt-5">
+          <label className="form-control w-2/12 max-w-xs">
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full max-w-xs"
+            />
+            <div className="label">
+              <span className="label-text-alt">Input Quantity</span>
+            </div>
+          </label>
+          <button className="btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="h-6 w-6 mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+              />
+            </svg>
+            <span className="flex-1 text-left">Add to Cart</span>
+          </button>
         </div>
       </div>
     </div>
